82ac3b2243a46f3c226065ce5dc54350f77286a5
Shubh Agnihotri <152176606+DarkOrthodox@users.noreply.github.com>
2025-08-24 19:08:35 +0530

    added note in line 47

--- Data-Preprocessing-Python/data_preprocessing_tools.py
+++ Data-Preprocessing-Python/data_preprocessing_tools.py
@@ -44,6 +44,6 @@ print(y_test)
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
-X_test[:, 3:] = sc.transform(X_test[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:]) # Note: Use transform() on test data to avoid data leakage
 print(X_train)
 print(X_test)
